cc20826c2b1bf0b5ff87d509713a26862b421aaa
GoudetOlivier <olivier.goudet@gmail.com>
2017-09-02 10:49:36 +0200

    Update run_CGNN_graph.py

--- run_CGNN_graph.py
+++ run_CGNN_graph.py
@@ -8,12 +8,12 @@ cgnn.SETTINGS.NB_GPU = 2
 cgnn.SETTINGS.NB_JOBS = 8
 
 #Setting for CGNN-Fourier
-cgnn.SETTINGS.use_Fast_MMD = True
-cgnn.SETTINGS.NB_RUNS = 64 
+#cgnn.SETTINGS.use_Fast_MMD = True
+#cgnn.SETTINGS.NB_RUNS = 64 
 
 #Setting for CGNN-MMD
-# cgnn.SETTINGS.use_Fast_MMD = False
-#cgnn.SETTINGS.NB_RUNS = 32
+cgnn.SETTINGS.use_Fast_MMD = False
+cgnn.SETTINGS.NB_RUNS = 32
 
 
 datafile = "Example_graph_numdata.csv"
