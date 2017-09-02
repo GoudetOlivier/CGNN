b52aca3b1331f209e12198c26dbf1106017add1d
GoudetOlivier <olivier.goudet@gmail.com>
2017-09-02 09:27:21 +0200

    Update Loss.py

--- Code/cgnn/utils/Loss.py
+++ Code/cgnn/utils/Loss.py
@@ -7,66 +7,34 @@ Date : 09/03/2017
 import tensorflow as tf
 import numpy as np
 
-bandwiths_gamma = [0.01, 0.1, 0.5,1, 2, 10, 100]
+bandwiths_gamma = [0.005, 0.05, 0.25, 0.5, 1, 5, 50]
 
-def MMD_loss_tf(xy_true, xy_pred, low_memory_version=False):
+def MMD_loss_tf(xy_true, xy_pred):
 
     N, _ = xy_pred.get_shape().as_list()
 
-    if (low_memory_version == 1):
+    X = tf.concat([xy_pred, xy_true], 0)
+    XX = tf.matmul(X, tf.transpose(X))
+    X2 = tf.reduce_sum(X * X, 1, keep_dims=True)
+    exponent = -2*XX + X2 + tf.transpose(X2)
 
-        loss = 0.0
+    s1 = tf.constant(1.0 / N, shape=[N, 1])
+    s2 = -tf.constant(1.0 / N, shape=[N, 1])
+    s = tf.concat([s1, s2], 0)
+    S = tf.matmul(s, tf.transpose(s))
 
-        XX = tf.matmul(xy_pred, tf.transpose(xy_pred))
-        X2 = tf.reduce_sum(xy_pred * xy_pred, 1, keep_dims=True)
-        exponentXX = XX - 0.5 * X2 - 0.5 * tf.transpose(X2)
-        sXX = tf.constant(1.0 / N ** 2, shape=[N, 1])
+    loss = 0
 
-        for i in range(len(bandwiths_gamma)):
-            kernel_val = tf.exp(1.0 / bandwiths_gamma[i] * exponentXX)
-            loss += tf.reduce_sum(sXX * kernel_val)
-
-        YY = tf.matmul(xy_true, tf.transpose(xy_true))
-        Y2 = tf.reduce_sum(xy_true * xy_true, 1, keep_dims=True)
-        exponentYY = YY - 0.5 * Y2 - 0.5 * tf.transpose(Y2)
-        sYY = tf.constant(1.0 / N ** 2, shape=[N, 1])
-
-        for i in range(len(bandwiths_gamma)):
-            kernel_val = tf.exp(1.0 / bandwiths_gamma[i] * exponentYY)
-            loss += tf.reduce_sum(sYY * kernel_val)
-
-        XY = tf.matmul(xy_pred, tf.transpose(xy_true))
-        exponentXY = XY - 0.5 * X2 - 0.5 * tf.transpose(Y2)
-        sXY = -tf.constant(2.0 / N ** 2, shape=[N, 1])
-
-        for i in range(len(bandwiths_gamma)):
-            kernel_val = tf.exp(bandwiths_gamma[i] * exponentXY)
-            loss += tf.reduce_sum(sXY * kernel_val)
-
-    else:
-
-        X = tf.concat([xy_pred, xy_true], 0)
-        XX = tf.matmul(X, tf.transpose(X))
-        X2 = tf.reduce_sum(X * X, 1, keep_dims=True)
-        exponent = XX - 0.5 * X2 - 0.5 * tf.transpose(X2)
-
-        s1 = tf.constant(1.0 / N, shape=[N, 1])
-        s2 = -tf.constant(1.0 / N, shape=[N, 1])
-        s = tf.concat([s1, s2], 0)
-        S = tf.matmul(s, tf.transpose(s))
-
-        loss = 0
-
-        for i in range(len(bandwiths_gamma)):
-            kernel_val = tf.exp(bandwiths_gamma[i] * exponent)
-            loss += tf.reduce_sum(S * kernel_val)
+    for i in range(len(bandwiths_gamma)):
+        kernel_val = tf.exp(-bandwiths_gamma[i] * exponent)
+        loss += tf.reduce_sum(S * kernel_val)
 
     return tf.sqrt(loss)
 
 
 def rp(k,s,d):
 
-  return tf.transpose(tf.concat([tf.concat([si*tf.random_normal([k,d], mean=0, stddev=1) for si in s], axis = 0), 2*np.pi*tf.random_normal([k*len(s),1], mean=0, stddev=1)], axis = 1))
+  return tf.transpose(tf.concat([tf.concat([2*si*tf.random_normal([k,d], mean=0, stddev=1) for si in s], axis = 0), 2*np.pi*tf.random_normal([k*len(s),1], mean=0, stddev=1)], axis = 1))
 
 def f1(x,wz,N):
 
