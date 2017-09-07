3fd6abbf0b745edfeec943259d8d844130e9ca9d
Diviyan Kalainathan <dkalainathan@gmail.com>
2017-09-07 17:02:53 +0200

    Fix Generator import

--- Code/cgnn/__init__.py
+++ Code/cgnn/__init__.py
@@ -2,11 +2,12 @@ import tensorflow as tf
 from .utils.Graph import DirectedGraph, UndirectedGraph
 from .CGNN import CGNN
 from .CGNN_confounders import CGNN_confounders
+from .CGNN import CGNNGenerator
 from .GNN import GNN
 from .generators import __init__
 from .utils import Loss
 from .utils.Settings import SETTINGS
 
 
-__all__ = ['DirectedGraph', 'UndirectedGraph', 'CGNN', 'CGNN_confounders', 'GNN']
+__all__ = ['DirectedGraph', 'UndirectedGraph', 'CGNN', 'CGNN_confounders', 'GNN', "CGNNGenerator"]
 
