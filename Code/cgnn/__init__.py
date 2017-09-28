02795017e7d793406024162386daf8fa682ad284
Diviyan Kalainathan <dkalainathan@gmail.com>
2017-09-28 15:11:30 +0200

    Added print filter option

--- Code/cgnn/__init__.py
+++ Code/cgnn/__init__.py
@@ -7,7 +7,10 @@ from .GNN import GNN
 from .generators import __init__
 from .utils import Loss
 from .utils.Settings import SETTINGS
+import os
+# avoid the thousands of stderr output lines
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
-__all__ = ['DirectedGraph', 'UndirectedGraph', 'CGNN', 'CGNN_confounders', 'GNN']
-
+__all__ = ['DirectedGraph', 'UndirectedGraph',
+           'CGNN', 'CGNN_confounders', 'GNN', 'CGNN_decomposable']
